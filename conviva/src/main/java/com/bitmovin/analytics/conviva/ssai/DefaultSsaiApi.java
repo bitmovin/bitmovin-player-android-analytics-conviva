24995573899f886a8b1c153871663bb8c0bcb2d9
Lukas Knoch-Girstmair <lukas.knoch-girstmair@bitmovin.com>
2024-07-03 10:01:53 +0200

    Reset potential ads and ad break on reset

--- conviva/src/main/java/com/bitmovin/analytics/conviva/ssai/DefaultSsaiApi.java
+++ conviva/src/main/java/com/bitmovin/analytics/conviva/ssai/DefaultSsaiApi.java
@@ -35,7 +35,8 @@ public class DefaultSsaiApi implements SsaiApi {
     }
 
     public void reset() {
-        isAdBreakActive = false;
+        reportAdFinished();
+        reportAdBreakFinished();
     }
 
     @Override
