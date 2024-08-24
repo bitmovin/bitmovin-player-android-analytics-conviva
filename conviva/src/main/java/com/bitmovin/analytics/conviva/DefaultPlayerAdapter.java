7df10b62856399bf89ab5346f415acd39250c8f4
David Steinacher <david.steinacher@bitmovin.com>
2024-08-24 11:49:27 +0200

    remove additional bitrate reporting
    * fixes failing unit test

--- conviva/src/main/java/com/bitmovin/analytics/conviva/DefaultPlayerAdapter.java
+++ conviva/src/main/java/com/bitmovin/analytics/conviva/DefaultPlayerAdapter.java
@@ -33,7 +33,6 @@ public class DefaultPlayerAdapter implements PlayerAdapter {
         VideoQuality playbackVideoData = player.getPlaybackVideoData();
         if (playbackVideoData != null) {
             videoData.put(ConvivaSdkConstants.PLAYBACK.RESOLUTION, new Object[]{playbackVideoData.getWidth(), playbackVideoData.getHeight()});
-            videoData.put(ConvivaSdkConstants.PLAYBACK.BITRATE, new Object[]{playbackVideoData.getBitrate() / 1000});
             final int peakBitrate = playbackVideoData.getPeakBitrate();
             if (peakBitrate != VideoQuality.BITRATE_NO_VALUE) {
                 videoData.put(ConvivaSdkConstants.PLAYBACK.BITRATE, new Object[]{peakBitrate / 1000});
