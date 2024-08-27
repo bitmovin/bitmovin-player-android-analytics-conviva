c6d4ab366a003e77c98067cf109fa3a8f3ff7ca1
David Steinacher <david.steinacher@bitmovin.com>
2024-08-27 11:38:49 +0200

    remove leftover after merge

--- conviva/src/main/java/com/bitmovin/analytics/conviva/ssai/WithEventEmitter.java
+++
@@ -1,8 +0,0 @@
-package com.bitmovin.analytics.conviva.ssai;
-
-import com.bitmovin.player.api.event.Event;
-import com.bitmovin.player.api.event.JavaEventEmitter;
-
-public interface WithEventEmitter {
-    void call(JavaEventEmitter<Event> player);
-}
